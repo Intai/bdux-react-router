8bf4ddc3fec90a43960117f7f726a37e161c1bad
Intai Huang <intai.huang@vodafone.com>
2016-08-22 18:30:19 +1200

    Fix to be compatible with bdux 0.1.23

--- src/actions/location-action.js
+++ src/actions/location-action.js
@@ -1,101 +1,123 @@
-import R from 'ramda';
-import Bacon from 'baconjs';
-import Common from '../utils/common-util';
-import Storage from '../utils/storage-util';
-import ActionTypes from './action-types';
-import { createHistory, createMemoryHistory } from 'history';
-import { bindToDispatch } from 'bdux';
+import R from 'ramda'
+import Bacon from 'baconjs'
+import Common from '../utils/common-util'
+import Storage from '../utils/storage-util'
+import ActionTypes from './action-types'
+import { createHistory, createMemoryHistory } from 'history'
+import { bindToDispatch } from 'bdux'
 
 const createPlatformHistory = R.ifElse(
   Common.canUseDOM,
   createHistory,
   createMemoryHistory
-);
+)
 
 const getHistory = (() => {
-  let history;
+  let history
   return () => (
     (history) ? history
       : (history = createPlatformHistory())
-  );
-})();
+  )
+})()
 
 const currentLocation = (() => {
-  let prev = {};
+  let prev = {}
   return (location) => (
     (location)
       ? (prev = location)
       : prev
-  );
-})();
+  )
+})()
 
 const getCurrentLocation = R.partial(
   currentLocation, [undefined]
-);
+)
 
 const cloneLocation = R.pick(
   ['pathname', 'search', 'state']
-);
+)
 
 const isEqual = R.useWith(
   R.equals, [
     cloneLocation,
     cloneLocation
   ]
-);
+)
 
 const isCurrentLocation = R.converge(
   isEqual, [
     R.identity,
     getCurrentLocation
   ]
-);
+)
 
 const createLocation = (location) => (
   getHistory().createLocation(location)
-);
+)
 
 const mapLocation = R.ifElse(
   R.is(String),
   createLocation,
   R.identity
-);
+)
 
 const updateHistory = R.curry((action, location) => {
   // if updating to a different location.
   if (!isCurrentLocation(location)) {
     // push or replace with the new location.
-    getHistory()[action](cloneLocation(location));
+    getHistory()[action](cloneLocation(location))
   }
-});
+})
 
 const onceThenNull = (func) => {
-  let count = 0;
+  let count = 0
   return (...args) => (
     (count++ <= 0)
       ? func.apply(func, args)
       : null
-  );
-};
+  )
+}
 
 const pushLocation = R.curry((sink, location) => {
-  sink(location);
-});
+  sink(location)
+})
 
 const removeHistorySession = (location) => {
-  Storage.remove('@@History/' + location.key);
-};
+  Storage.remove('@@History/' + location.key)
+}
 
 const shouldDispatchAction = R.complement(
   R.allPass([
     R.is(Object),
     R.path(['state', 'skipAction'])
   ])
-);
+)
+
+const skipFirst = func => {
+  let count = 0
+  return (...args) => {
+    if (count++ > 0) {
+      func.apply(func, args)
+    }
+  }
+}
+
+const createBinder = (() => {
+  let count = 0
+
+  return sink => (
+    // if creating binder to reactivate the bacon stream.
+    (count++ > 0)
+      // skip the current location.
+      ? skipFirst(pushLocation(sink))
+      // otherwise sink the current location.
+      : pushLocation(sink)
+  )
+})()
 
 export const listen = onceThenNull(() => (
   Bacon.fromBinder((sink) => {
-    return getHistory().listen(pushLocation(sink));
+    return getHistory().listen(createBinder(sink))
   })
   // remove session entry created by history library.
   .doAction(removeHistorySession)
@@ -108,20 +130,20 @@ export const listen = onceThenNull(() => (
   .map(R.merge({
     type: ActionTypes.ROUTE_LOCATION_UPDATE
   }))
-));
+))
 
 export const push = R.pipe(
   mapLocation,
   updateHistory('push')
-);
+)
 
 export const replace = R.pipe(
   mapLocation,
   updateHistory('replace')
-);
+)
 
 export default bindToDispatch({
   listen,
   push,
   replace
-});
+})
