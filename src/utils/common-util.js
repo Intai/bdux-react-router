b75050eb7f3f6618a5008704407425588a72b229
intai <intai.hg@gmail.com>
2017-01-07 14:50:17 +1300

    Add unit tests for common utilities

--- src/utils/common-util.js
+++ src/utils/common-util.js
@@ -1,31 +1,37 @@
-import R from 'ramda';
+import R from 'ramda'
 
-const PREFIX = 'RL';
+const PREFIX = 'BDUXRL'
 
 const mapToKeyValue = (obj, key) => {
-  obj[key] = PREFIX + '_' + key;
+  obj[key] = PREFIX + '_' + key
   return obj
-};
+}
 
-const canUseDOM = R.once(() => (
+export const canUseDOM = () => (
   typeof window !== 'undefined'
     && window.document
     && window.document.createElement
-));
+)
+
+const canUseDOMOnce = R.once(
+  canUseDOM
+)
+
+export const deferOnClient = (pred) => (func) => (...args) => {
+  if (pred()) {
+    setTimeout(R.partial(func, args), 1)
+  }
+}
 
 export default {
 
-  canUseDOM,
+  canUseDOM: canUseDOMOnce,
 
-  deferOnClient: (func) => (...args) => {
-    if (canUseDOM()) {
-      setTimeout(R.partial(func, args), 1);
-    }
-  },
+  deferOnClient: deferOnClient(canUseDOMOnce),
 
   // map an array of strings to
   // object keys and prefixed values.
-  createObjOfConsts: R.reduce(
-    mapToKeyValue, {}
+  createObjOfConsts: (values) => R.reduce(
+    mapToKeyValue, {}, values
   )
-};
+}
---
+++ src/utils/common-util.spec.js
@@ -0,0 +1,82 @@
+/* eslint-env mocha */
+
+import chai from 'chai'
+import sinon from 'sinon'
+import Common, {
+  canUseDOM,
+  deferOnClient } from './common-util'
+
+describe('Common Utilities', () => {
+
+  let clock
+
+  beforeEach(() => {
+    clock = sinon.useFakeTimers()
+  })
+
+  it('should not be able to use dom when there is no window', () => {
+    global.window = undefined
+    chai.expect(canUseDOM()).to.not.be.ok
+  })
+
+  it('should not be able to use dom when there is no document', () => {
+    global.window = { document: undefined }
+    chai.expect(canUseDOM()).to.not.be.ok
+  })
+
+  it('should not be able to use dom when there is no function to create element', () => {
+    global.window = { document: { createElement: undefined }}
+    chai.expect(canUseDOM()).to.not.be.ok
+  })
+
+  it('should be able to use dom when there is function to create element', () => {
+    global.window = { document: { createElement: () => {} }}
+    chai.expect(canUseDOM()).to.be.ok
+  })
+
+  it('should cache whether dom is available', () => {
+    global.window = undefined
+    const cached = Common.canUseDOM()
+    global.window = { document: { createElement: () => {} }}
+    chai.expect(Common.canUseDOM()).to.equal(cached)
+  })
+
+  it('should generate an object of constants', () => {
+    const storeNames = Common.createObjOfConsts(['ROUTER'])
+    chai.expect(storeNames).to.eql({
+      ROUTER: 'BDUXRL_ROUTER'
+    })
+  })
+
+  it('should defer on client', () => {
+    const callback = sinon.stub()
+    global.window = { document: { createElement: () => {} }}
+    deferOnClient(canUseDOM)(callback)('test')
+    clock.tick(1)
+    chai.expect(callback.calledOnce).to.be.true
+    chai.expect(callback.lastCall.args[0]).to.equal('test')
+  })
+
+  it('should not defer on server', () => {
+    const callback = sinon.stub()
+    global.window = undefined
+    deferOnClient(canUseDOM)(callback)()
+    clock.tick(1)
+    chai.expect(callback.called).to.be.false
+  })
+
+  it('should cache whether to defer', () => {
+    const callback = sinon.stub()
+    global.window = undefined
+    Common.canUseDOM()
+    global.window = { document: { createElement: () => {} }}
+    Common.deferOnClient(callback)()
+    clock.tick(1)
+    chai.expect(callback.called).to.be.false
+  })
+
+  afterEach(() => {
+    clock.restore()
+  })
+
+})
