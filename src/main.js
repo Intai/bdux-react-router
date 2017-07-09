e9f63d81d70879859b4bbd4548cb72f48e7fa27a
intai <intai.hg@gmail.com>
2017-07-09 21:32:16 +1200

    Export switch as well

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 export { default as LocationAction } from './actions/location-action'
 export { default as LocationStore } from './stores/location-store'
 export { default as Link } from './components/link'
-export { Router, Route } from 'react-router'
+export { Router, Switch, Route } from 'react-router'
 export * from './location-history'
