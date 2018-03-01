2e75e462642ba0e7c649e050c35284e21f1a167c
intai <intai.hg@gmail.com>
2018-03-01 23:26:40 +1300

    Fix ramda import

--- src/location-history.js
+++ src/location-history.js
@@ -1,4 +1,4 @@
-import R from 'ramda'
+import * as R from 'ramda'
 import Common from './utils/common-util'
 import LocationAction from './actions/location-action'
 import { createMemoryHistory } from 'history'
