2e378213292b0525520ee9e7d7794804235f3ac0
intai <intai.hg@gmail.com>
2022-05-05 18:24:28 +1200

    Remove createLocationHistory

--- src/location-history.js
+++ src/location-history.js
@@ -4,37 +4,7 @@ import {
   tap,
 } from 'ramda'
 import Common from './utils/common-util'
-import { isLocationEqual } from './utils/location-util'
 import * as LocationAction from './actions/location-action'
-import { createMemoryHistory } from 'history'
-
-const getPathname = (location) => (
-  (typeof location === 'string')
-    ? location
-    : (location ? location.pathname : null)
-)
-
-const createInitialEntries = (pathname) => (
-  (pathname)
-    ? { initialEntries: [pathname] }
-    : {}
-)
-
-const createHistory = pipe(
-  getPathname,
-  createInitialEntries,
-  createMemoryHistory
-)
-
-const historyProp = (() => {
-  let history
-
-  return {
-    setHistory: location => history = createHistory(location),
-    getHistory: location => history || (history = createHistory(location)),
-    updateHistory: location => history && location && history.replace(location, { skipAction: true })
-  }
-})()
 
 const addLocationState = assocPath(
   ['state', 'skipAction'],
@@ -55,26 +25,12 @@ const deferUpdateLocation = (location) => {
 }
 
 export const resetLocationHistory = (location) => {
-  // force to recreate history.
-  historyProp.setHistory(location)
   // and update location store.
   LocationAction.reset(location)
 }
 
-export const createLocationHistory = (location) => {
-  // initialise history if hasn't.
-  // always return the same react-router history
-  // because it does not support changing history.
-  const history = historyProp.getHistory(location)
-
-  if (!isLocationEqual(location, history.location)) {
-    // react-router v4 doesn't update component
-    // with the location sent through history listen.
-    // workaround by modifying history itself.
-    historyProp.updateHistory(location)
-    // defer to be after render.
-    deferUpdateLocation(location)
-  }
-
-  return history
+export const updateRouterLocation = (location) => {
+  // defer to be after render.
+  deferUpdateLocation(location)
+  return location || ''
 }
--- src/location-history.spec.js
+++ src/location-history.spec.js
@@ -5,8 +5,9 @@ import sinon from 'sinon'
 import Common from './utils/common-util'
 import * as LocationAction from './actions/location-action'
 import {
-  createLocationHistory,
-  resetLocationHistory } from './location-history'
+  resetLocationHistory,
+  updateRouterLocation,
+} from './location-history'
 
 describe('Location History', () => {
 
@@ -20,79 +21,18 @@ describe('Location History', () => {
     sandbox.restore()
   })
 
-  it('should create a location history object', () => {
-    const history = createLocationHistory(null)
-    chai.expect(history).to.have.property('listen')
-      .and.is.a('function')
+  it('should have a default', () => {
+    const location = updateRouterLocation(null)
+    chai.expect(location).to.equal('')
   })
 
-  it('should have a default current location', () => {
-    const history = createLocationHistory(null)
-    chai.expect(history.location).to.include({
-      pathname: '/',
-      search: '',
-      hash: '',
-      state: null
-    })
-  })
-
-  it('should keep the same history object', () => {
-    const history1 = createLocationHistory(null)
-    const history2 = createLocationHistory({})
-    chai.expect(history1).to.equal(history2)
-  })
-
-  it('should set the current location', () => {
-    sandbox.stub(LocationAction, 'reset')
-    resetLocationHistory({ pathname: '/test' })
-    const history = createLocationHistory(undefined)
-    chai.expect(history.location).to.include({
-      pathname: '/test',
-      search: '',
-      hash: '',
-      state: null
-    })
-  })
-
-  it('should set the current pathname', () => {
-    sandbox.stub(LocationAction, 'reset')
-    resetLocationHistory('/pathname')
-    const history = createLocationHistory(undefined)
-    chai.expect(history.location).to.include({
-      pathname: '/pathname',
-      search: '',
-      hash: '',
-      state: null
-    })
-  })
-
-  it('should replace location thourgh action to reset ', () => {
+  it('should replace location through action to reset ', () => {
     sandbox.stub(LocationAction, 'reset')
     resetLocationHistory({ pathname: '/test' })
     chai.expect(LocationAction.reset.calledOnce).to.be.true
     chai.expect(LocationAction.reset.lastCall.args[0]).to.eql({
       pathname: '/test'
     })
-  })
-
-  it('should defer update on client', () => {
-    const deferOnClient = sandbox.stub(Common, 'deferOnClient')
-    createLocationHistory({})
-    chai.expect(deferOnClient.calledOnce).to.be.true
-    chai.expect(deferOnClient.lastCall.args[1]).to.eql({})
-  })
-
-  it('should not update without location', () => {
-    sandbox.stub(Common, 'deferOnClient')
-    createLocationHistory(null)
-    chai.expect(Common.deferOnClient.called).to.be.false
-  })
-
-  it('should not update the same location', () => {
-    sandbox.stub(Common, 'deferOnClient')
-    createLocationHistory({ pathname: '/test/same' })
-    createLocationHistory({ pathname: '/test/same' })
-    chai.expect(Common.deferOnClient.callCount).to.equal(1)
   })
 
   describe('on client', () => {
@@ -104,36 +44,22 @@ describe('Location History', () => {
       })
     })
 
-    it('should replace location thourgh action', () => {
-      resetLocationHistory()
-      createLocationHistory({})
+    it('should defer on client', () => {
+      updateRouterLocation({})
+      chai.expect(Common.deferOnClient.calledOnce).to.be.true
+      chai.expect(Common.deferOnClient.lastCall.args[1]).to.eql({})
+    })
+
+    it('should not defer without location', () => {
+      updateRouterLocation(null)
+      chai.expect(Common.deferOnClient.called).to.be.false
+    })
+
+    it('should replace through action', () => {
+      resetLocationHistory('/pathname')
+      updateRouterLocation({})
       chai.expect(LocationAction.replace.calledOnce).to.be.true
       chai.expect(LocationAction.replace.lastCall.args[0]).to.eql({
-        state: {
-          skipAction: true
-        }
-      })
-    })
-
-    it('should send location to listener', () => {
-      const history = createLocationHistory(null)
-      const listener = sinon.stub()
-      history.listen(listener)
-      createLocationHistory({ pathname: '/test/listener' })
-      chai.expect(listener.calledOnce).to.be.true
-      chai.expect(listener.lastCall.args[0].location).to.deep.include({
-        pathname: '/test/listener',
-        state: {
-          skipAction: true
-        }
-      })
-    })
-
-    it('should update location along with listener', () => {
-      const history = createLocationHistory(null)
-      createLocationHistory({ pathname: '/test/variable' })
-      chai.expect(history.location).to.deep.include({
-        pathname: '/test/variable',
         state: {
           skipAction: true
         }
